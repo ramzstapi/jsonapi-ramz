3327ffbc115e574ef66b460dd6177fb42ab68e54
ramadityo <97384153+ramadityo@users.noreply.github.com>
2022-01-16 21:21:55 +0800

    Update gabut.js

--- assets/js/gabut.js
+++ assets/js/gabut.js
@@ -1,12 +1,15 @@
-const animationRubber = document.querySelector(".animating-rubberBand");
-animationRubber.addEventListener("mouseout", function () {
-  setTimeout(function () {
-    animationRubber.classList.remove(
-      "animate__animated",
-      "animate__rubberBand"
-    );
-  }, 1200);
-});
-animationRubber.addEventListener("mouseover", function () {
-  animationRubber.classList.add("animate__animated", "animate__rubberBand");
+const animationRubber = document.querySelectorAll(".animating-rubberBand");
+
+animationRubber.forEach(function(animasiRubber){
+  animasiRubber.addEventListener("mouseout", function () {
+    setTimeout(function () {
+      animasiRubber.classList.remove(
+        "animate__animated",
+        "animate__rubberBand"
+      );
+    }, 1200);
+  });
+  animasiRubber.addEventListener("mouseover", function () {
+    animasiRubber.classList.add("animate__animated", "animate__rubberBand");
+  });
 });
