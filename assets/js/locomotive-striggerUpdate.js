ad63bbeab0942878c4987a2dd9318b25cfec8d76
Newbie Coder <97384153+ramadityo@users.noreply.github.com>
2022-01-28 09:18:34 +0800

    Update locomotive-striggerUpdate.js

--- assets/js/locomotive-striggerUpdate.js
+++ assets/js/locomotive-striggerUpdate.js
@@ -7,13 +7,13 @@ gsap.registerPlugin(ScrollTrigger);
 
 const locoScroll = new LocomotiveScroll({
   el: document.querySelector(".viewport"),
-  smooth: true
-//   smartphone: {
-//     smooth: true
-//   },
-//   tablet: {
-//     smooth: true
-//   }
+  smooth: true,
+  smartphone: {
+    smooth: true
+  },
+  tablet: {
+    smooth: true
+  }
 });
 // each time Locomotive Scroll updates, tell ScrollTrigger to update too (sync positioning)
 locoScroll.on("scroll", ScrollTrigger.update);
