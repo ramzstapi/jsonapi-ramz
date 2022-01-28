d16e611ddcc5b2e0c8c58d7da9b0eef3719557aa
Newbie Coder <97384153+ramadityo@users.noreply.github.com>
2022-01-28 18:00:47 +0800

    Update locomotive-striggerUpdate.js

--- assets/js/locomotive-striggerUpdate.js
+++ assets/js/locomotive-striggerUpdate.js
@@ -9,10 +9,10 @@ const locoScroll = new LocomotiveScroll({
   el: document.querySelector(".viewport"),
   smooth: true,
   smartphone: {
-    smooth: false
+    smooth: true
   },
   tablet: {
-    smooth: false
+    smooth: true
   }
 });
 // each time Locomotive Scroll updates, tell ScrollTrigger to update too (sync positioning)
