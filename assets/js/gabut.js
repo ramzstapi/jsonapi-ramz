06a3e8fb5ddfcf4c686fb1c77646121a9e985450
Newbie Coder <97384153+ramadityo@users.noreply.github.com>
2022-01-31 17:51:04 +0800

    Update gabut.js

--- assets/js/gabut.js
+++ assets/js/gabut.js
@@ -13,3 +13,12 @@ animationRubber.forEach(function(animasiRubber){
     animasiRubber.classList.add("animate__animated", "animate__rubberBand");
   });
 });
+
+new hoverEffect({
+  parent: document.querySelector('.hEffect'),
+  intensity: 0.3,
+  image1: '../../media/photos/hadeh.jpg',
+  image2: '../../media/photos/hadeh2.jpg',
+  displacementImage: '../../media/photos/disss.png',
+  imagesRatio: 400 / 400
+})
